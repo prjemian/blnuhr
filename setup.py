890afa55beec038776e6c6ad5bb3eeae0c000f53
jemian <prjemian@gmail.com>
2016-01-14 23:38:26 -0600

    add starter for standalone GUI

--- setup.py
+++ setup.py
@@ -19,26 +19,27 @@ verbose=1
 long_description = open('README.rst', 'r').read()
 
 
-setup (name             = blnuhr.__package_name__,        # blnuhr
-       version          = blnuhr.__version__,
-       license          = blnuhr.__license__,
-       description      = blnuhr.__description__,
-       long_description = long_description,
-       author           = blnuhr.__author_name__,
-       author_email     = blnuhr.__author_email__,
-       url              = blnuhr.__url__,
-       download_url     = blnuhr.__download_url__,
-       keywords         = blnuhr.__keywords__,
-       install_requires = requires,
-       platforms        = 'any',
-       package_dir      = {'': 'src'},
-       #packages         = find_packages(),
-       packages         = [str(blnuhr.__package_name__), ],
-       package_data     = dict(blnuhr=['resources/*', ]),
-       classifiers      = blnuhr.__classifiers__,
-#        entry_points={
-#           # create & install console_scripts in <python>/bin
-#           #'console_scripts': ['blnuhr = blnuhr.main:main'],
-#           'gui_scripts': ['blnuhr = blnuhr.main:main'],
-#           },
+setup (
+    name             = blnuhr.__package_name__,        # blnuhr
+    version          = blnuhr.__version__,
+    license          = blnuhr.__license__,
+    description      = blnuhr.__description__,
+    long_description = long_description,
+    author           = blnuhr.__author_name__,
+    author_email     = blnuhr.__author_email__,
+    url              = blnuhr.__url__,
+    download_url     = blnuhr.__download_url__,
+    keywords         = blnuhr.__keywords__,
+    install_requires = requires,
+    platforms        = 'any',
+    package_dir      = {'': 'src'},
+    #packages         = find_packages(),
+    packages         = [str(blnuhr.__package_name__), ],
+    package_data     = dict(blnuhr=['resources/*', ]),
+    classifiers      = blnuhr.__classifiers__,
+    entry_points={
+       # create & install console_scripts in <python>/bin
+       #'console_scripts': ['blnuhr = blnuhr.main:main'],
+       'gui_scripts': ['blnuhr = blnuhr:main'],
+       },
       )
